847ccada45f79769d361ebfc6a440a8e9ea59ebe
DOUGLAS RAFAEL FERREIRA <douglas_ferreira@edu.unifil.br>
2025-10-17 00:26:57 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,10 @@
 // This file is intentionally left blank. 
 // The Firebase configuration will be populated automatically.
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  "projectId": "studio-4846636938-cb362",
+  "appId": "1:801004562279:web:28150d2b8ca87e785a31c2",
+  "apiKey": "AIzaSyBq1G3Qzx3umBICuCiOtD2OwuSR8C7J9eE",
+  "authDomain": "studio-4846636938-cb362.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "801004562279"
+};
